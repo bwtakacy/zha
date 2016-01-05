f1c7a8babd163b69d032a3600cc09c4342d0de62
bwtakacy <bwtakacy@gmail.com>
2016-01-05 15:40:59 +0900

    [impl_pgsql]check the replication status on health check script

--- impl_pgsql/check_health.sh
+++ impl_pgsql/check_health.sh
@@ -4,6 +4,10 @@ then
   if [ $? = 0 ] ; then exit 3; fi
   exit 0
 fi
-/usr/pgsql-9.4/bin/psql -c "select 1" > /dev/null 2>&1
-if [ $? = 0 ] ; then exit 2; fi
+ret=0
+psql -c "select 1" > /dev/null 2>&1
+if [ $? = 0 ] ; then ret=1; fi
+state=`psql -c "select sync_state from pg_stat_replication" -t 2> /dev/null`
+if [ -z "$state" ] ; then exit $ret; fi
+[[ $state == 'async' || $state == 'sync' ]] ; exit 2
 exit 0
